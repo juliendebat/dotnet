f974fe402cd8e025c46644a51a3d5df149e325e3
Piercuta <39705712+Piercuta@users.noreply.github.com>
2022-01-06 14:22:00 +0100

    Delete stopApplication.sh

--- .aws/stopApplication.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "stop application"
-cd  /var/www/website/
-echo "kill $(ps aux | grep ChoixResto.dll | awk '{print $2}')"
-kill $(ps aux | grep ChoixResto.dll | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
-rm -rf /var/www/website/
